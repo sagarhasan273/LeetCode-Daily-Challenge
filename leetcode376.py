1e61ae6738f5c33e2dc116bb6299f16c858e100e
sagarhasan273 <sagarhasan273@gmail.com>
2022-07-03 17:15:15 +0600

    Linear DP O(n) time and O(1) space

--- leetcode376.py
+++ leetcode376.py
@@ -1,17 +1,12 @@
 class Solution:
     def wiggleMaxLength(self, nums: List[int]) -> int:
         n = len(nums)
-        up, down = [0]*n, [0]*n
+        up, down = 1,1
         
         for i in range(1, n):
             if nums[i] > nums[i-1]:
-                up[i] = down[i-1]+1
-                down[i] = down[i-1]
+                up = down+1
             elif nums[i] < nums[i-1]:
-                down[i] = up[i-1]+1
-                up[i] = up[i-1]
-            else:
-                up[i] = up[i-1]
-                down[i] = down[i-1]
-        
-        return max(up[-1], down[-1]) + 1
+                down = up+1
+
+        return max(up, down)
