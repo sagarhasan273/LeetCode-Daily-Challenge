55dcc1e0900a275ce8930e3a0b657e2eef155314
sagarhasan273 <sagarhasan273@gmail.com>
2022-07-04 21:40:10 +0600

    294 ms, faster than 30.06% of Python3 online submissions for Candy.

---
+++ leetcode135.py
@@ -0,0 +1,15 @@
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        left = [1]
+        for i in range(1, len(ratings)):
+            if ratings[i] > ratings[i-1]:
+                left.append(left[-1] + 1)
+            else:
+                left.append(1) 
+        
+        right = [1] * len(ratings)
+        for i in range(len(ratings)-2, -1, -1):
+            if ratings[i] > ratings[i+1]:
+                right[i] = right[i+1] + 1
+
+        return sum([max(l, r) for l, r in zip(left, right)])
