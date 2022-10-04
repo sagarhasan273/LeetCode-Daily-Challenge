227780ce3b29407a8eb50e310ba79dfc2830cbad
sagarhasan273 <sagarhasan273@gmail.com>
2022-10-04 14:22:00 +0600

    84 ms, faster than 36.29% of Python3 online submissions for Path Sum.

---
+++ 112. Path Sum.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if not root: return False
+        
+        def dfs(node, total):
+            if not node:
+                return False
+            
+            if not node.left and not node.right:
+                return total+node.val == targetSum
+            
+            return dfs(node.left, total + node.val) or dfs(node.right, total + node.val)
+        
+        return dfs(root, 0)
+        
